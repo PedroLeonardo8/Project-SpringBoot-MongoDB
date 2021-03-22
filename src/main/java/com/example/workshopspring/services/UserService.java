f769fe414a2ac03591533b9950ba70ed7c62d0cc
PedroLeonardo8 <Pedroleonardo841@gmail.com>
2021-03-22 15:41:56 -0300

    FindById e suas exceções

--- src/main/java/com/example/workshopspring/services/UserService.java
+++ src/main/java/com/example/workshopspring/services/UserService.java
@@ -1,20 +1,28 @@
 package com.example.workshopspring.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopspring.domain.User;
 import com.example.workshopspring.repositories.UserRepository;
+import com.example.workshopspring.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
