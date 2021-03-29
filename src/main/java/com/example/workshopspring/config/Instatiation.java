9d70c9a7193acb4b1ae2e40e3467d71bf17b79a8
PedroLeonardo8 <Pedroleonardo841@gmail.com>
2021-03-29 15:47:55 -0300

    Acrescentando Comentários aos Posts

--- src/main/java/com/example/workshopspring/config/Instatiation.java
+++ src/main/java/com/example/workshopspring/config/Instatiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.example.workshopspring.domain.Post;
 import com.example.workshopspring.domain.User;
 import com.example.workshopspring.dto.AuthorDTO;
+import com.example.workshopspring.dto.CommentDTO;
 import com.example.workshopspring.repositories.PostRepository;
 import com.example.workshopspring.repositories.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instatiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("2018/03/21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("2018/03/23"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("2018/03/21"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("2018-03-22"), new AuthorDTO(roberta));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("2018-03-23"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+				
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
